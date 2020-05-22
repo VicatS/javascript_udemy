bd3b184337a34f94b7e0dad6cd070acbfd0bd9d5
victor <victorcs2704@gmail.com>
2020-05-21 22:31:45 -0400

    29. Eventos de raton y teclado

--- Clase 26/scripts.js
+++ Clase 26/scripts.js
@@ -13,49 +13,47 @@ Eventos de teclado:
     keyup - cuando soltamos una tecla
     keypress - cuando pulsamos una tecla y no la soltamos
 */
-
+// eventos de raton
 const button = document.getElementById('button')
-const input = document.getElementById('input')
 const box = document.getElementById('box')
+const input = document.getElementById('input')
 
-/* button.addEventListener('click', () => {
+/*button.addEventListener('click', () => {
     console.log('CLICK')
-}) */
+})*/
 
-/* button.addEventListener('dblclick',()=> {
+/*button.addEventListener('dblclick', () => {
     console.log('DOBLE CLICK')
-}) */
+})*/
 
-/* box.addEventListener('mouseenter', () => {
+/*box.addEventListener('mouseenter', () => {
     box.classList.replace('red', 'green')
-
 })
 
 box.addEventListener('mouseleave', () => {
     box.classList.replace('green', 'red')
-}) */
+})*/
 
-/* box.addEventListener('mousedown', () => {
-    console.log('HAS PULSADO EN LA CAJA');
+/*box.addEventListener('mousedown', () => {
+    console.log('HAS PULSADO EN LA CAJA')
 })
 
 box.addEventListener('mouseup', () => {
-  console.log('HAS SOLTADO EL BOTON IZQUIERDO EN LA CAJA')
-}) */
+    console.log('HAS SOLTADO EL BOTON IZQUIERDO EN LA CAJA')
+})*/
 
-/* box.addEventListener('mousemove', () => {
-    console.log('ESTAS MOVIENDO EL RATON EN LA CAJA');
-}) */
+/*box.addEventListener('mousemove', () => {
+    console.log('ESTAS MOVIENDO EL RATON EN LA CAJAS')
+})*/
 
-/* input.addEventListener('keydown', () => {
-    console.log('HAS PULSADO UNA TECLA');
+input.addEventListener('keydown', () => {
+    console.log('HAS PULSADO UNA TECLA')
 })
 
 input.addEventListener('keyup', () => {
-  console.log('HAS SOLTADO UNA TECLA')
+    console.log('HAS SOLTADO UNA TECLA')
 })
 
 input.addEventListener('keypress', () => {
-  console.log('ESTÁS PULSANDO UNA TECLA')
-}) */
-
+    console.log('ESTAS PULSANDO UNA TECLA')
+})
