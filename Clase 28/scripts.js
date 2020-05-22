a119cc89c8cbacad1f272e05fb6e8ecb431ef962
victor <victorcs2704@gmail.com>
2020-05-21 23:34:21 -0400

    31. Crear e insertar elementos

--- Clase 28/scripts.js
+++ Clase 28/scripts.js
@@ -10,33 +10,37 @@ Añadir un elemento al DOM: parent.appendChild(element)
 Fragmentos de código: document.createDocumentFragment()
 */
 
-const days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
+const days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo', 'Valor Extra']
 
 const title = document.getElementById('title')
 const daysList = document.getElementById('daysList')
 const selectDays = document.getElementById('daysSelect')
 
-/* const itemList = document.createElement('LI')
+/*const itemList = document.createElement('LI')
 itemList.textContent = 'Lunes'
 
-daysList.appendChild(itemList) */
+daysList.appendChild(itemList)*/
 
-title.innerHTML = 'DOM - <span>Crear e insertar elementos I<span>'
+title.innerHTML = 'DOM - <span>Crear e insertar elementos I</span>'
 
+/*
 const fragment = document.createDocumentFragment()
 
-/* for (const day of days) {
+for (const day of days) {
     const itemList = document.createElement('LI')
     itemList.textContent = day
     fragment.appendChild(itemList)
-} */
+}
+
+daysList.appendChild(fragment)*/
+
+const fragment = document.createDocumentFragment()
 
 for (const day of days) {
     const selectItem = document.createElement('OPTION')
-    selectItem.setAttribute('value', day.toLowerCase())
+    selectItem.setAttribute('value', day.toLocaleLowerCase())
     selectItem.textContent = day
     fragment.appendChild(selectItem)
 }
 
-/* daysList.appendChild(fragment) */
 selectDays.appendChild(fragment)
