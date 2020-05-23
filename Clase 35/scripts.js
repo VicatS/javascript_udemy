0a8265131125c5a0dee4f256c85c934ebb0bb4f7
victor <victorcs2704@gmail.com>
2020-05-23 19:25:47 -0400

    38. Callbacks

--- Clase 35/scripts.js
+++ Clase 35/scripts.js
@@ -1,45 +1,44 @@
 //Un callback es una función que se ejecuta a través de otra función
 //Los callbacks no son asíncronos
 
-/* const getUser = (id, cb) => {
+/*
+const getUser = (id, cb) => {
     const user = {
-        name: 'Dorian',
+        name: 'Victor',
         id: id
     }
-
     if (id == 2) cb('User not exist')
     else cb(null, user)
 }
 
 getUser(1, (err, user) => {
     if (err) return console.log(err)
-    console.log(`User name is ${user.name}`);
-}) */
+    console.log(`User name is ${user.name}`)
+})*/
 
 const users = [
     {
         id: 1,
-        name: 'Dorian'
-
+        name: 'Victor'
     },
     {
         id: 2,
-        name: 'Laura'
+        name: 'Geovana'
     },
     {
         id: 3,
-        name: 'Carlos'
+        name: 'Lidia'
     }
 ]
 
 const emails = [
     {
         id: 1,
-        email: 'dorian@gmail.com'
+        email: 'victor@gmail.com'
     },
     {
         id: 2,
-        email: 'laura@gmail.com'
+        email: 'geovana@gmail.com'
     }
 ]
 
@@ -51,7 +50,7 @@ const getUser = (id, cb) => {
 
 const getEmail = (user, cb) => {
     const email = emails.find(email => email.id == user.id)
-    if (!email) cb(`${user.name} hasn't email`)
+    if (!email) cb(`${user.name} has not email`)
     else cb(null, {
         id: user.id,
         name: user.name,
@@ -61,18 +60,9 @@ const getEmail = (user, cb) => {
 
 getUser(3, (err, user) => {
     if (err) return console.log(err)
-
-    getEmail(user, (err, res) => {
+    getEmail(user, (err,res) => {
         if (err) return console.log(err)
-        getEmail(user, (err, res) => {
-            if (err) return console.log(err)
-            getEmail(user, (err, res) => {
-                if (err) return console.log(err)
-                getEmail(user, (err, res) => {
-                    if (err) return console.log(err)
-                    console.log(res);
-                })
-            })
-        })
+        console.log(res)
     })
 })
+
