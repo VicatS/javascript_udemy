25a8af8ed815b194d2871ec7b0230bbb8a1fa9cc
victor <victorcs2704@gmail.com>
2020-05-25 17:31:05 -0400

    43. async / await

--- Clase 40/scripts.js
+++ Clase 40/scripts.js
@@ -1,25 +1,32 @@
-/* const getName = async () => {
+/*
+const getName = async () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve('Dorian')
+            resolve('Victor')
         }, 1500)
+
     })
 }
+
+getName()
+    .then(name => console.log(name))
 
 const sayHello = async () => {
     const name = await getName()
     return `Hello ${name}`
 }
 
-sayHello().then(res=>console.log(res)) */
+sayHello()
+    .then(res => console.log(res))*/
 
-const users = [{ id: 1, name: 'Dorian' }, { id: 2, name: 'Laura' }, { id: 3, name: 'Carlos' }]
-const emails = [{ id: 1, email: 'dorian@gmail.com' }, { id: 2, email: 'laura@gmail.com' }]
+const users = [{id: 1, name: 'Dorian'}, {id: 2, name: 'Laura'}, {id: 3, name: 'Carlos'}]
+const emails = [{id: 1, email: 'dorian@gmail.com'}, {id: 2, email: 'laura@gmail.com'}]
 
 const getUser = async (id) => {
     const user = users.find(user => user.id == id)
-    if (!user) throw new Error(`Doesn't exist an user with id ${id}`)
+    if (!user) throw new Error(`Dont't exist an user with id ${id}`)
     else return user
+
 }
 
 const getEmail = async (user) => {
@@ -29,17 +36,24 @@ const getEmail = async (user) => {
         id: user.id,
         name: user.name,
         email: email.email
+
     })
 }
+
+/*const getInfo = () => {
+    return getEmail(getUser(1))
+}*/
 
 const getInfo = async (id) => {
     try {
         const user = await getUser(id)
         const res = await getEmail(user)
-        return `${user.name} email is ${res.email}`
+        return `${user.name} your email es ${res.email}`
     } catch (error) {
         console.log(error)
     }
+
 }
 
-getInfo(3).then(res => console.log(res))
+getInfo(3)
+    .then(res => console.log(res))
