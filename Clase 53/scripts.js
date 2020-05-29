79df7f4533631535901f9de8b7f02a898a6bfbb5
victor <victorcs2704@gmail.com>
2020-05-28 21:27:41 -0400

    56. API File-Barra de progreso

--- Clase 53/scripts.js
+++ Clase 53/scripts.js
@@ -1,19 +1,21 @@
 const fileInput = document.getElementById('file')
 const progress = document.getElementById('progress')
 
-// fileInput.addEventListener('change', (e) => {
-//     const file = e.target.files[0]
-//     const fileReader = new FileReader()
-//     fileReader.readAsDataURL(file)
+/*fileInput.addEventListener('change', (e) => {
+    const file = e.target.files[0]
+    const fileReader = new FileReader()
+    fileReader.readAsDataURL(file)
 
-//     fileReader.addEventListener('progress', (e) => {
-//         progress.style.width = Number.parseInt(e.loaded * 100 / e.total) + '%'
-//     })
+    fileReader.addEventListener('progress', (e) => {
+        /!*console.log(e.loaded *100 / e.total)
+        console.log(e.total)*!/
+        progress.style.width = Number.parseInt(e.loaded * 100 / e.total ) + '%'
+    })
 
-//     fileReader.addEventListener('loadend', () => {
-//         progress.style.width = '100%'
-//     })
-// })
+    fileReader.addEventListener('loadend', () => {
+        progress.style.width = '100%'
+    })
+})*/
 
 const root = document.documentElement
 
@@ -23,10 +25,11 @@ fileInput.addEventListener('change', (e) => {
     fileReader.readAsDataURL(file)
 
     fileReader.addEventListener('progress', (e) => {
-        root.style.setProperty('--bar-width', Number.parseInt(e.loaded * 100 / e.total) + '%')
+       root.style.setProperty('--bar-width', Number.parseInt(e.loaded * 100 / e.total) + '%')
     })
 
     fileReader.addEventListener('loadend', () => {
         root.style.setProperty('--bar-width', '100%')
     })
 })
+
