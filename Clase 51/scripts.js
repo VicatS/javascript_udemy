aa4058fde8c5339ddd821c7d1fb7feb6785c17e6
victor <victorcs2704@gmail.com>
2020-05-28 20:20:28 -0400

    54. Drag & Drop II

--- Clase 51/scripts.js
+++ Clase 51/scripts.js
@@ -3,26 +3,42 @@ const finishedTasks = document.getElementById('finished-tasks')
 //dataTransfer
 //setData: Establece la información que queremos compartir
 //getData: Establece la información que queremos obtener
-pendingTasks.addEventListener('dragstart', (e) => {
-    e.dataTransfer.setData('text/plain', e.target.id)
-})
 
+pendingTasks.addEventListener('dragstart', (e) => {
+    // console.log(e.target)
+    e.dataTransfer.setData('text/plain', e.target.id)
+    // e.dataTransfer.getData('text')
+})
 pendingTasks.addEventListener('drag', (e) => {
     e.target.classList.add('active')
 })
-
 pendingTasks.addEventListener('dragend', (e) => {
     e.target.classList.remove('active')
 })
 
-//OBLIGATORIO, SI NO, NO FUNCIONA
+// OBLIGATORIO PARA FUNCIONAR
 finishedTasks.addEventListener('dragover', (e) => {
     e.preventDefault()
 })
-
 finishedTasks.addEventListener('drop', (e) => {
     e.preventDefault()
     const element = document.getElementById(e.dataTransfer.getData('text'))
     element.classList.remove('active')
     finishedTasks.appendChild(pendingTasks.removeChild(element))
 })
+
+finishedTasks.addEventListener('dragstart', (e) => {
+    // console.log(e.target)
+    e.dataTransfer.setData('text/plain', e.target.id)
+    console.log(e.dataTransfer.getData('text'))
+
+})
+
+pendingTasks.addEventListener('drop', (e) => {
+    e.preventDefault()
+    const element2 = document.getElementById(e.dataTransfer.getData('text'))
+    element2.classList.remove('active')
+    pendingTasks.appendChild(finishedTasks.removeChild(element2))
+})
+
+
