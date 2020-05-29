d3628d23a584e0d8cdf8140edceeab1e6cffe6d8
victor <victorcs2704@gmail.com>
2020-05-28 21:06:01 -0400

    55. API File

--- Clase 52/scripts.js
+++ Clase 52/scripts.js
@@ -11,27 +11,31 @@ const img = document.getElementById('img')
 const images = document.getElementById('images')
 const text = document.getElementById('text')
 
-// fileInput.addEventListener('change', (e) => {
-//     const file = e.target.files[0]
-//     const fileReader = new FileReader()
-//     fileReader.readAsText(file)
-//     fileReader.addEventListener('load', (e) => {
-//         text.textContent = e.target.result
-//     })
-// })
-
-//Carga simple de imagen
-// fileInput.addEventListener('change', (e) => {
-//     const file = e.target.files[0]
-//     const fileReader = new FileReader()
-//     fileReader.readAsDataURL(file)
-//     fileReader.addEventListener('load', (e) => {
-//         img.setAttribute('src', e.target.result)
-//     })
-// })
-
-//Carga múltiple de imágenes
+/*
 fileInput.addEventListener('change', (e) => {
+    // console.log(e.target.files)
+    const file = e.target.files[0]
+    const fileReader = new FileReader()
+    fileReader.readAsText(file)
+    fileReader.addEventListener('load', (e) => {
+        text.textContent = e.target.result
+    })
+})*/
+
+/*//Carga simple de imagen
+fileInput.addEventListener('change', (e) => {
+    // console.log(e.target.files)
+    const file = e.target.files[0]
+    const fileReader = new FileReader()
+    fileReader.readAsDataURL(file)
+    fileReader.addEventListener('load', (e) => {
+        img.setAttribute('src', e.target.result)
+    })
+})*/
+
+//Carga multiple de imagen
+fileInput.addEventListener('change', (e) => {
+    // console.log(e.target.files)
     const files = e.target.files
     const fragment = document.createDocumentFragment()
     for (const file of files) {
